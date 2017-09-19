60afe4933121bc856d3494cd948ddbe8d8ce7603
Danny Vega <dannyevega@gmail.com>
2017-09-19 12:23:40 -0700

    more tests

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ describe("Variables", function() {
 
 });
 
-describe("Train", function() {
+describe("Los Angeles Train", function() {
 
 	it("should have traveled 380 miles", function(){
 		assert.equal(app.losAngeles.distanceTraveled, 380);
@@ -37,8 +37,27 @@ describe("Train", function() {
 
 });
 
+describe("San Franisco Train", function() {
+
+	it("should have 100 seats available", function(){
+		assert.equal(app.sanFrancisco.seats, 380);
+	})
+	
+	it("should have traveled 0 miles", function(){
+		assert.equal(app.sanFrancisco.distanceTraveled, 0);
+	})	
+	
+	it("should throw an error because there arent enough seats", function(){
+		assert.equal(app.sanFrancisco.reserveSeats(104), error);
+	})	
+
+});
+
 // for mocha testing
 losAngeles.run("San Francisco"); // distanceTraveled for losAngeles train should be 380
 losAngeles.reserveSeats(9); // seats on losAngeles train should be 91
 losAngeles.remainingSeats() // should be 91
 losAngeles.returnSeats() // should be back at 100
+sanFrancisco.seats // should equal 100
+sanFrancisco.distanceTraveled // should equal 0
+sanFrancisco.reserveSeats(102) // should throw error
